d0220e6262e07c8dd2b6984ef929b68121061ed9
AsymiLinkMeta <Pat@Asymilinkmeta.com>
2026-09-07 03:27:01 -0700

    Fix react-query provider setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "@/lib/auth/provider";
+import { AppProviders } from "@/components/es/providers";
 import { ScrollManager } from "@/components/es/scroll-manager";
 import "./es.css";
 import "./index.css";
@@ -11,8 +12,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
-        <ScrollManager />
+        <AppProviders>
+          <App />
+          <ScrollManager />
+        </AppProviders>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
